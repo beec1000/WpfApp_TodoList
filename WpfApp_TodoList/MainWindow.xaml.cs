acc31884ad8b046f1c032ce1b540c5a941c8a643
beec1000 <beec1000.minecraft@gmail.com>
2024-04-09 13:15:16 +0200

    Add project files.

---
+++ WpfApp_TodoList/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_TodoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (List.Items.Count > 0)
+            {
+                DeleteButton.IsEnabled = true;
+            }
+        }
+
+        private void Upload_Button(object sender, RoutedEventArgs e)
+        {
+            string newData = Data.Text;
+            List.Items.Add(newData);
+            newData = string.Empty;
+
+            if (List.Items.Contains(newData))
+            {
+                MessageBox.Show("You already have this on the list!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(newData))
+            {
+                MessageBox.Show("You don't have any data!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Delete_Button(object sender, RoutedEventArgs e)
+        {
+            List.Items.Clear();
+        }
+    }
+}
