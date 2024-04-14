94fd6c7c379521d692af61f2c768e420790e24ce
beec1000 <beec1000.minecraft@gmail.com>
2024-04-14 22:54:54 +0200

    u

--- WpfApp_TodoList/MainWindow.xaml.cs
+++ WpfApp_TodoList/MainWindow.xaml.cs
@@ -1,52 +1,80 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_TodoList
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
 
-            if (List.Items.Count > 0)
-            {
-                DeleteButton.IsEnabled = true;
-            }
+            UpdateButtons();
         }
 
-        private void Upload_Button(object sender, RoutedEventArgs e)
+        private void UpdateButtons()
+        {
+            DeleteButton.IsEnabled = List.SelectedItems.Count > 0;
+            ModifyButton.IsEnabled = List.SelectedItems.Count > 0;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             string newData = Data.Text;
-            List.Items.Add(newData);
-            newData = string.Empty;
-
-            if (List.Items.Contains(newData))
-            {
-                MessageBox.Show("You already have this on the list!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
 
             if (string.IsNullOrEmpty(newData))
             {
-                MessageBox.Show("You don't have any data!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Please enter a data to add to list", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (List.Items.Contains(newData))
+            {
+                MessageBox.Show("This data is already on the list", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            List.Items.Add(newData);
+            Data.Clear();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (List.SelectedItems.Count > 0)
+            {
+                while (List.SelectedItems.Count > 0)
+                {
+                    List.Items.Remove(List.SelectedItems[0]);
+                }
+
+                UpdateButtons();
             }
         }
 
-        private void Delete_Button(object sender, RoutedEventArgs e)
+        private void ModifyButton_Click(object sender, RoutedEventArgs e)
         {
-            List.Items.Clear();
+            if (List.SelectedIndex != -1)
+            {
+                string updatedData = Data.Text;
+
+                if (string.IsNullOrEmpty(updatedData))
+                {
+                    MessageBox.Show("Please enter data to modify", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                List.Items[List.SelectedIndex] = updatedData;
+                Data.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please select data to modify", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ListSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateButtons();
         }
     }
 }
